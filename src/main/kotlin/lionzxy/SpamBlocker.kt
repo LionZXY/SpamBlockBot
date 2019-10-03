e24e055c2569c66d28618eb6201150193c8138e0
LionZXY <nikita@kulikof.ru>
2019-10-03 16:04:31 +0300

    Add xinvst.com and send nickname

--- src/main/kotlin/lionzxy/SpamBlocker.kt
+++ src/main/kotlin/lionzxy/SpamBlocker.kt
@@ -14,9 +14,9 @@ val DENIED_NICKNAME = listOf("hoimsufa235", "kladov_oper", "klds_bot24", "restor
         "restoransamara", "restoranoren", "tribogabot", "lab05bot", "st_smr", "restorantlt", "BitCapitan",
         "durman001_bot", "NEWSinfinityshop_biz", "nurtime1", "udnewbot", "prtekb", "prtufablag",
         "prtorenorsk", "is_op", "oper_iso24", "pharmstd_chat",
-        "bgd_pharmstd", "msk_pharmstd", "krk_pharmstd", "rzn_pharmstd", "MDKLAD3", "Fiksik86", "cannabioz_cosmos", "BestModerator").map { it.toLowerCase() }.toHashSet()
+        "bgd_pharmstd", "msk_pharmstd", "krk_pharmstd", "rzn_pharmstd", "MDKLAD3", "Fiksik86", "cannabioz_cosmos", "BestModerator", "qymyz07", "Oraku1").map { it.toLowerCase() }.toHashSet()
 
-val DENIED_URL = listOf("bit.ly", "t.me", "t.cn", "tinyurl.com", "autofleet.bz")
+val DENIED_URL = listOf("bit.ly", "t.me", "t.cn", "tinyurl.com", "autofleet.bz", "xinvst.com")
 
 object SpamBlocker {
     val chatMap = HashMap<String, ListWithLimit<Message>>()
